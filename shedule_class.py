8640e2771c15ad180824aa9438ff782b89554cdd
LaironLF <dimaklassk@gmail.com>
2022-10-13 20:20:10 +0700

    fix

--- shedule_class.py
+++ shedule_class.py
@@ -1,11 +1,10 @@
-from hashlib import new
 from watchdog.events import FileSystemEventHandler
 def substruct(a: str):
         a = a.split("\\")
         a = a[len(a) - 1].split(".")
         return a[0]
+    
 def change(a: str):
-    newa=''
     for i in a:
         if(i.lower()=='a' or i.lower()=='e' or i.lower()=='i' or i.lower()=='o' or i.lower()=='u' or i.lower()=='y' or i.lower()=='i' or i.lower()=='а' or i.lower()=='е' or i.lower()=='ё' or i.lower()=='и' or i.lower()=='о' or i.lower()=='у'  or i.lower()=='ы' or i.lower()=='э' or i.lower()=='ю' or i.lower()=='я'):
             print(i.lower())
